7bcb7a8cef57818f07722d9f9f9b64b61fa51699
Xiaowang0229 <wangjiaxu20120229@163.com>
2026-09-12 07:23:20 +0800

    Init:初始化各页面

---
+++ ARCRemake/Views/Pages/ListDianMingPage.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ARCRemake;
+
+public partial class ListDianMingPage : UserControl
+{
+    public ListDianMingPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ ARCRemake/Views/Pages/SettingsPage.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace ARCRemake;
+
+public partial class SettingsPage : UserControl
+{
+    public SettingsPage()
+    {
+        InitializeComponent();
+    }
+}

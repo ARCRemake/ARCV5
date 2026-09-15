9576f84cc02f0ce28b7396a0cbb39eccb9da1efe
Xiaowang0229 <wangjiaxu20120229@163.com>
2026-09-16 06:50:33 +0800

    Update:Dianming-p1

--- ARCRemake/Views/Pages/DianMingPage.axaml.cs
+++ ARCRemake/Views/Pages/DianMingPage.axaml.cs
@@ -6,6 +6,8 @@ using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
 using Avalonia.Threading;
 using FluentAvalonia.UI.Navigation;
+using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace ARCRemake;
@@ -13,11 +15,23 @@ namespace ARCRemake;
 public partial class DianMingPage : UserControl
 {
     private bool DianmingStatus = false;
+
+    private List<string> Fullnamelist;
+    private List<string> Pickernamelist;
     private DispatcherTimer DMTimer;
+    private Random randomname = new Random();
     public DianMingPage()
     {
         InitializeComponent();
-
+        var a2 = JsonServices.ReadJson<AppConfig>($"{Environment.CurrentDirectory}/Config.json");
+        var timer = new DispatcherTimer();
+        timer.Interval = TimeSpan.FromMilliseconds(a2.IntervalTick);
+        timer.Tick += (s, e) =>
+        {
+            NameBlock.Text = Fullnamelist[randomname.Next(0, Fullnamelist.Count - 1)];
+        };
+        DMTimer = timer;
+        
     }
 
     private async void Page_Loaded(object s, RoutedEventArgs e)
@@ -33,22 +47,52 @@ public partial class DianMingPage : UserControl
             ListDianMingBox.IsVisible = true;
             NameBlock.IsVisible = false;
         }
-        var timer = new DispatcherTimer();
-        DMTimer = timer;
+        var a2 = JsonServices.ReadJson<AppConfig>($"{Environment.CurrentDirectory}/Config.json");
+        
+        Fullnamelist = JsonServices.ReadJson<NameListConfig>(a2.CurrentNameListPath).Names;
+        Pickernamelist = JsonServices.ReadJson<NameListConfig>(a2.CurrentNameListPath).Names;
+
+        
     }
     private async void Page_Unloaded(object s, RoutedEventArgs e)
     {
-        DianmingStatus = false;
-        
+        if(DianmingStatus)
+        {
+            Button_ChangeStatus();
+        }
+        NameBlock.Text = "请开始点名";
+        Fullnamelist.Clear();
+        Pickernamelist.Clear();
     }
 
     private void RootButton_Click(object s, RoutedEventArgs e)
     {
-
+        Button_ChangeStatus();
     }
 
     private void Button_ChangeStatus()
     {
+        switch(RootClasses.DianMingMode)
+        {
+            case "随机点名":
+                if (DianmingStatus)
+                {
+                    DMTimer.Stop();
+                    DianmingStatus = false;
+                }
+                else
+                {
+                    DianmingStatus = true;
+                }
+                break;
+            case "立即点名":
+                break;
+            case "批量点名":
+                break;
+            case "定时点名":
+                break;
+        }
+
 
     }
 }
